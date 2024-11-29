c5f9058cf2082607ee28754bf3056ef97322a627
Perm <wannuwatbuayangyuen.gmail.com>
2024-11-29 16:49:30 +0700

    lab6_1.cpp completed

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -2,8 +2,26 @@
 using namespace std;
 
 int main(){
+    int i = 0;
+    int x;
+    int Odd = 0;
+    int Even =0;
+    while(i == 0){
     cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
+    cin >> x;
+    if(x == 0){
+        break;
+    }
+    if(x % 2 == 0){
+        Even++;
+    }
+    
+    if(x % 2 != 0){
+        Odd++;
+    }
+
+    }
+    cout << "#Even numbers = " << Even  << "\n" ;
+    cout << "#Odd numbers = " << Odd;
     return 0;
 }
